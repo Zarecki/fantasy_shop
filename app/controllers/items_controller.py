144c537d4288351f92974d4f20aaaea473ce9dfe
Zarecki <jonzarecki@gmail.com>
2020-12-11 13:29:25 +0000

    basic item routes written

--- app/controllers/items_controller.py
+++ app/controllers/items_controller.py
@@ -20,6 +20,12 @@ def show_item(id):
     return render_template("items/show.html", item=item)
 
 # NEW
+@items_blueprint.route("/items/new")
+def new_item():
+    manufacturers = manufacturer_repository.select_all()
+    return render_template("items/new.html", manufacturers=manufacturers)
+
+# CREATE
 @items_blueprint.route("/items", methods=["POST"])
 def create_item():
     name = request.form["name"]
@@ -32,4 +38,32 @@ def create_item():
     stock = request.form["stock"]
     new_item = Item(name, description, category, buy_cost, sell_price, manufacturer, stock)
     item_repository.save(new_item)
+    return redirect("/items")
+
+# EDIT
+@items_blueprint.route("/items/<id>/edit")
+def edit_item(id):
+    item = item_repository.select(id)
+    manufacturers = manufacturer_repository.select_all()
+    return render_template("items/edit.html", item=item, manufacturers=manufacturers)
+
+# UPDATE
+@items_blueprint.route("/items/<id>", methods=["POST"])
+def update_item(id):
+    name = request.form["name"]
+    description = request.form["description"]
+    category = request.form["category"]
+    buy_cost = request.form["buy_cost"]
+    sell_price = request.form["sell_price"]
+    manufacturer_id = request.form["manufacturer_id"]
+    manufacturer = manufacturer_repository.select(manufacturer_id)
+    stock = request.form["stock"]
+    item = Item(name, description, category, buy_cost, sell_price, manufacturer, stock, id)
+    item_repository.update(item)
+    return redirect("/items")
+
+# DELETE
+@items_blueprint.route("items/<id>/delete")
+def delete_item(id):
+    item_repository.delete(id)
     return redirect("/items")
